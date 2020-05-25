70eb719f2fda416f7d6d1569258e72f7b77b831d
Jude Okagu <juud0010@gmail.com>
2020-05-25 15:42:50 +0200

    working on UI

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,31 +4,38 @@ import home from '../images/home.png';
 const HomePage = () => {
   return (
     <div className='main-container'>
-      <p>Where will you like to Live</p>
-      <form action='/action_page.php'>
-        <label htmlFor='cars'>Choose Location:</label>
-        <select name='location' id='location'>
-          <option value='Toronto'>Toronto</option>
-          <option value='Montreat'>Montreat</option>
-          <option value='Ottawa'>Ottawa</option>
-          <option value='Victoria'>Victoria</option>
-        </select>
-        <br />
-        <br />
-        {/* <input type='submit' defaultValue='Submit' /> */}
-      </form>
+      <p>
+        Search our listings for entire or shared homes with<br></br> flexible
+        durations and payments plans.
+      </p>
 
       <h1>
         The best way to<br></br> rent your home
       </h1>
-      <p1>
-        Search our listings for entire or shared homes with<br></br> flexible
-        durations and payments plans.
-      </p1>
-
-      {/* <div className='img'>
-        <img src={home} alt='Home' />
-      </div> */}
+      <div className='dropdown'>
+        <button
+          className='btn btn-primary dropdown-toggle'
+          type='button'
+          data-toggle='dropdown'
+        >
+          Choose Location
+          {/* <span className='caret' /> */}
+        </button>
+        <ul className='dropdown-menu'>
+          <li>
+            <a href='#'>Toronto</a>
+          </li>
+          <li>
+            <a href='#'>Vancouver</a>
+          </li>
+          <li>
+            <a href='#'>Ottawa</a>
+          </li>
+          <li>
+            <a href='#'>Montreal</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
